37c7475d37d4330483928cbe4e2a31c6f9888c7b
jksun <sjkyspa@gmail.com>
2016-03-14 23:26:28 +0800

    refactor the vim install

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
-PWD=$(pwd)
-BASEDIR=$(dirname $0)
+#!/bin/sh
+pushd $(dirname $0) > /dev/null
+BASEDIR=`pwd`
+popd > /dev/null
 # init the vundle
+pushd $BASEDIR
 git submodule init
 git submodule update
 
-echo $BASEDIR
 # create a symble link to home dir to use vimrc as the vim configure file
-ln -s $PWD/$BASEDIR/vimrc ~/.vimrc
-ln -s $PWD/$BASEDIR/vim ~/.vim
+ln -s $BASEDIR/vimrc ~/.vimrc
+ln -s $BASEDIR/vim ~/.vim
 
 # add Bundle plugins
 vim +BundleInstall +qall
 
+popd
