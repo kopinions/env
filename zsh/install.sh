ea336534107a61551a4a24e7f49d8263cac26d92
jksun <sjkyspa@gmail.com>
2016-03-15 00:17:40 +0800

    refactor the zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
-PWD=$(pwd)
-BASEDIR=$(dirname $0)
+#!/bin/sh
+pushd $(dirname $0) > /dev/null
+BASEDIR=`pwd`
+popd > /dev/null
 
+# remove the default config
+rm -rf ~/.zshrc
+rm -rf ~/.oh-my-zsh/
+
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s $PWD/$BASEDIR/zshrc ~/.zshrc
+ln -s $BASEDIR/zshrc ~/.zshrc
+
