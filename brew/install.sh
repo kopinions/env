0b5012d18653ec948072362b6764eb0c0d01d9e7
jksun <sjkyspa@gmail.com>
2016-03-14 23:01:14 +0800

    add brew install

--- brew/install.sh
+++ brew/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+pushd $(dirname $0) > /dev/null
+BASEDIR=`pwd`
+popd > /dev/null
 PWD=$(pwd)
-BASEDIR=$(dirname $0)
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 ln -s $PWD/$BASEDIR/Brewfile ~/.Brewfile
--- zsh/install.sh
+++ zsh/install.sh
@@ -2,4 +2,5 @@
 PWD=$(pwd)
 BASEDIR=$(dirname $0)
 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -s $PWD/$BASEDIR/zshrc ~/.zshrc
