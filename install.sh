d32d6fba57a818282a7a2cdbb862170d2cb4ea0a
jksun <sjkyspa@gmail.com>
2016-03-15 00:26:46 +0800

    rename the components by name order

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ rm -rf ~/.oh-my-env
 
 # init
 git clone https://github.com/sjkyspa/env.git ~/.oh-my-env
-find ~/.env -type f -name install.sh -depth 2 -exec sh {} \;
+find ~/.oh-my-env -type f -name install.sh -depth 2 |sort|xargs -I{} sh {}
