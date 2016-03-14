c9ac7e43cace82f18bd6ad48f1fc46c2d9ee2fec
jksun <sjkyspa@gmail.com>
2016-03-15 00:23:00 +0800

    refator

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-git clone https://github.com/sjkyspa/env.git ~/.env
+# clean from last install
+rm -rf ~/.oh-my-env
+
+# init
+git clone https://github.com/sjkyspa/env.git ~/.oh-my-env
 find ~/.env -type f -name install.sh -depth 2 -exec sh {} \;
