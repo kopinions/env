d79538e2c240f5ea11a8d52fb68a64e85c8a3fbe
sjkyspa <sjkyspa@gmail.com>
2016-03-15 10:55:40 +0800

    fix zsh install

--- 02-zsh/install.sh
+++ 02-zsh/install.sh
@@ -8,6 +8,7 @@ rm -rf ~/.zshrc
 rm -rf ~/.oh-my-zsh/
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
+rm -rf ~/.zshrc
 ln -s $BASEDIR/zshrc ~/.zshrc
 
