6934da5e6f0f8824a39cc132f51d74f56dc70422
sjkyspa <sjkyspa@gmail.com>
2019-01-28 17:15:01 +0800

    update git config

--- 07-git/install.sh
+++ 07-git/install.sh
@@ -5,6 +5,8 @@ popd > /dev/null
 
 # clean the git config
 rm -rf ~/.gitconfig || true
+rm -rf ~/.gitignore || true
 
 # init
 ln -s $BASEDIR/gitconfig ~/.gitconfig
+ln -s $BASEDIR/gitignore ~/.gitignore
