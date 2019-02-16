beb3664ddaefc22067d8994290c0e9aa1d6342a9
sjkyspa <sjkyspa@gmail.com>
2019-02-16 23:53:42 +0800

    add hotkeys for window

--- 03-hammerspoon/hammerspoon/init.lua
+++ 03-hammerspoon/hammerspoon/init.lua
@@ -5,58 +5,183 @@ require('functions')
 Install=spoon.SpoonInstall
 
 local log = hs.logger.new('init.lua', 'debug')
-local window = require 'hs.window'
-
-local newKeyEvent = require 'hs.eventtap'.event.newKeyEvent
-local usleep = require 'hs.timer'.usleep
-local hyper = {'cmd','ctrl'}
-local apps = {
-   {
-      name = 'Emacs',
-      im = 'EN'
-   },
-   {
-      name = 'Google Chrome',
-      im = 'EN'
-   },
-   {
-      name = 'Wechat',
-      im = 'CN'
-   },
-   {
-      name = 'OmniFocus',
-      im = 'CN'
-   },
-}
-
-local hotkeys = {
-   {
-      mods = {'cmd'},
-      key = 'space',
-      specs = {
-         {
-            name = "Emacs",
-            fn = function()
-               hs.keycodes.currentSourceID("com.apple.keylayout.US")
-               local app = window.focusedWindow():application()
-               newKeyEvent({"cmd"}, "space", true):post(app)
-               usleep(1000)
-               newKeyEvent({"cmd"}, "space", false):post(app)
-            end
-         },
-      },
-      default = function()
-         local sid = hs.keycodes.currentSourceID()
-         if (sid == "com.sogou.inputmethod.sogou.pinyin") then
-            hs.keycodes.currentSourceID("com.apple.keylayout.US")
-         else
-            hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
-         end
-      end
-   }
-}
 
 function hks(name, etype, app)
+   local windows = require("windows")
+   windows:init()
+   local hotkeys = {
+      {
+         mods = {'cmd'},
+         key = 'space',
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  hs.keycodes.currentSourceID("com.apple.keylayout.US")
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd"}, "space", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd"}, "space", false):post(app)
+               end
+            },
+         },
+         default = function()
+            local sid = hs.keycodes.currentSourceID()
+            if (sid == "com.sogou.inputmethod.sogou.pinyin") then
+               hs.keycodes.currentSourceID("com.apple.keylayout.US")
+            else
+               hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
+            end
+         end
+      },
+      {
+         mods = {"cmd", "ctrl"},
+         key = "Left",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Left", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Left", false):post(app)
+               end
+            }
+         },
+         default = function()
+            windows.leftHalf()
+         end
+      },
+      {
+         mods = {"cmd", "ctrl"},
+         key = "Right",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Right", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Right", false):post(app)
+               end
+            }
+         },
+         default = function()
+            windows.rightHalf()
+         end
+      },
+      {
+         mods = {"cmd", "ctrl"},
+         key = "Up",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Up", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Up", false):post(app)
+               end
+            }
+         },
+         default = function()
+            windows.topHalf()
+         end
+      },
+      {
+         mods = {"cmd", "ctrl"},
+         key = "Down",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Down", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl"}, "Down", false):post(app)
+               end
+            }
+         },
+         default = function()
+            windows.bottomHalf()
+         end
+      },
+      {
+         mods = {"cmd", "ctrl", "alt"},
+         key = "Up",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl", "alt"}, "Up", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl", "alt"}, "Up", false):post(app)
+               end
+            }
+         },
+         default = function()
+            windows.maximize()
+         end
+      },
+      {
+         mods = {"cmd", "ctrl", "alt"},
+         key = "f",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl", "alt"}, "f", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd", "ctrl", "alt"}, "f", false):post(app)
+               end
+            }
+         },
+         default = function()
+            windows.toggleMaximized()
+         end
+      },
+      {
+         mods = {"cmd", "alt"},
+         key = "c",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd", "alt"}, "c", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd", "alt"}, "c", false):post(app)
+               end
+            }
+         },
+         default = function()
+            windows.center()
+         end
+      },
+      {
+         mods = {"cmd"},
+         key = "\\",
+         specs = {
+            {
+               name = "Emacs",
+               fn = function()
+                  local app = hs.window.focusedWindow():application()
+                  hs.eventtap.event.newKeyEvent({"cmd"}, "\\", true):post(app)
+                  hs.timer.usleep(1000)
+                  hs.eventtap.event.newKeyEvent({"cmd"}, "\\", false):post(app)
+               end
+            },
+         },
+         default = function()
+            hs.eventtap.event.newKeyEvent({"cmd", "ctrl", "alt"}, "\\", true):post()
+            hs.timer.usleep(1000)
+            hs.eventtap.event.newKeyEvent({"cmd", "ctrl", "alt"}, "\\", false):post()
+         end
+      }
+   }
+
    if (etype == hs.application.watcher.activated) then
       for k, v in pairs (hotkeys) do
          local hk = filter(
@@ -64,9 +189,6 @@ function hks(name, etype, app)
                return string.match(name:lower(), item.name:lower())
             end,
             v.specs)
-         for k,v in pairs (hk) do
-            log.i ('kv', k, v)
-         end
 
          if next(hk) == nil then
             hs.hotkey.new(v.mods, v.key, nil,  v.default):enable()
@@ -76,22 +198,34 @@ function hks(name, etype, app)
       end
    elseif (etype == hs.application.watcher.deactivated) then
       for k, v in pairs (hotkeys) do
-         hs.hotkey.new(v.mods, v.key, nil,  hk[1].fn):disable()
+         --         hs.hotkey.disableAll(v.mods, v.key)
       end
    end
 end
 
 hs.application.watcher.new(hks):start()
 
-Install:updateAllRepos()
--- Binding key to start plugin.
-Install:installSpoonFromRepo("WindowHalfsAndThirds")
-local whs = hs.loadSpoon("WindowHalfsAndThirds")
-
-hs.spoons.use("WindowHalfsAndThirds", {hotkeys = 'default'})
-
 -- auto change the im for the application callback
 function ims(name, etype, app)
+   local apps = {
+      {
+         name = 'Emacs',
+         im = 'EN'
+      },
+      {
+         name = 'Google Chrome',
+         im = 'EN'
+      },
+      {
+         name = 'Wechat',
+         im = 'CN'
+      },
+      {
+         name = 'OmniFocus',
+         im = 'CN'
+      },
+   }
+
    if (etype == hs.application.watcher.activated) then
       config = filter(
          function(item)
@@ -111,5 +245,13 @@ end
 
 -- auto change the im for the application
 hs.application.watcher.new(ims):start()
+
+hs.application.watcher.new(
+   function ()
+      if (etype == hs.application.watcher.activated) then
+      elseif (etype == hs.application.watcher.deactivated) then
+      end
+   end
+):start()
 
 hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
